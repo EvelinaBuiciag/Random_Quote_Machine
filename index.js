8ebf44c15ed7a19d8ac976e8c52f0e32fda685ad
Evelina B <anysolutions@yahoo.com>
2016-09-02 13:14:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+if (window.location.protocol !== 'http:') url = 'https://crossorigin.me/' + url;
 var getQuote = function(data) {
   $(".quote-text").text(data.quoteText);
   var quot = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' Author ' + data.quoteAuthor;
